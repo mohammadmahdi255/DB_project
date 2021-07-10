528b454248132245a10d436226c6e50f6fcc441c
Mohammad Mahdi Nemati <adel110@aut.ac.ir>
2021-07-11 01:51:17 +0430

    add function to DBHelper

--- DB_command_controller.py
+++ DB_command_controller.py
@@ -1,3 +1,5 @@
+import datetime
+
 from mysql.connector import connect, Error
 
 
@@ -28,18 +30,46 @@ class DBHelper:
         except Error as e:
             print(e)
 
-    def check_login(self):
+    def __call_procedure(self, procedure_name, parameter):
+        answer = []
+        cursor = self.__connection.cursor()
         try:
-            cursor = self.__connection.cursor()
-            check_login = """SELECT * FROM USER_DATA; SELECT * FROM USER_DATA; """
-            cursor.execute(check_login, multi=True)
-            res = cursor.fetchall()
-            for row in res:
-                print(row)
-            cursor.close()
-            self.__connection.commit()
+            answer.extend(cursor.callproc(procedure_name, args=parameter))
+            for result in cursor.stored_results():
+                answer.extend(result.fetchall())
         except Error as e:
             print(e)
+        finally:
+            cursor.close()
+            self.__connection.commit()
+
+        return answer
+
+    def check_login(self):
+        return self.__call_procedure('check_login', (0,))
+
+    def user_log(self, user_name, password, login=1):
+        return self.__call_procedure('user_log', (user_name, password, login, 0))
+
+    def add_ava(self, ava_content, post_datetime):
+        return self.__call_procedure('add_ava', (ava_content, post_datetime, 0))
+
+    def add_hashtag(self, text):
+        return self.__call_procedure('add_hashtag', (text, 0))
+
+    def add_hashtag_to_ava(self, ava_id, text):
+        return self.__call_procedure('add_hashtag_to_ava', (ava_id, text, 0))
+
+    def add_user(self, first_name, last_name, user_name,
+                 password, birthday, register_day, biography):
+        return self.__call_procedure('add_user', (first_name, last_name, user_name,
+                                                  password, birthday, register_day, biography))
+
+    def ava_comments(self, receiver_user_name, receiver_ava_id, content):
+        return self.__call_procedure('ava_comments', (receiver_user_name, receiver_ava_id, content, 0))
+
+    def block_user(self, blocked_user_name):
+        return self.__call_procedure('block_user', (blocked_user_name, 0))
 
 
 s1 = DBHelper(host="localhost",
@@ -47,4 +77,12 @@ s1 = DBHelper(host="localhost",
               password="",
               database="db_project")
 
-s1.check_login()
+print(s1.user_log('USER4', 'USER0002', 1))
+print(s1.check_login())
+print(s1.add_hashtag('#ABCu0'))
+print(s1.add_hashtag_to_ava(3, '#ABCu3'))
+print(s1.add_user('FN3', 'LN3', 'USER3', 'USER0003',
+                  datetime.datetime(2000, 10, 13, 10, 0, 0), datetime.datetime.now(), 'i am FN3'))
+
+print(s1.ava_comments('USER1', 2, 'this ok!'))
+print(s1.block_user('USER4'))
